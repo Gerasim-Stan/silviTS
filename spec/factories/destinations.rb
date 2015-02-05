c38ee9377a252a6af5cb9ce306af801822df5d2f
Gero <gero@tutuf.com>
2015-02-05 15:04:13 +0200

    Add spec for destinations controller

--- spec/factories/destinations.rb
+++ spec/factories/destinations.rb
@@ -4,4 +4,9 @@ FactoryGirl.define do
   factory :destination do
     name { Faker::Address.city }
   end
+
+  factory :invalid_destination, parent: :destination do
+    id nil
+    name nil
+  end
 end
