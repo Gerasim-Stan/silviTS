13c1a1804c05b1296c608f98997b695db942086e
Gero <gero@tutuf.com>
2015-02-13 16:27:14 +0200

    Add associations to destination and event for trip factory

--- spec/factories/trips.rb
+++ spec/factories/trips.rb
@@ -5,8 +5,8 @@ FactoryGirl.define do
     date { Faker::Date.between(1000.days.ago, 1000.days.from_now) }
     hour { Faker::Time.between(1000.days.ago, 1000.days.from_now) }
     starting_point { Faker::Address.street_name }
-    destination_id (- 1)
-    event_id (- 1)
+    association :destination, factory: :destination
+    association :event, factory: :event
   end
 
   factory :invalid_arguments, parent: :trip do
