80aa701d6e79e5aecd8b58773757f0558fc99156
Gero <gero@tutuf.com>
2015-02-13 15:20:46 +0200

    Add invalid trip factory

--- spec/factories/trips.rb
+++ spec/factories/trips.rb
@@ -8,5 +8,14 @@ FactoryGirl.define do
     destination_id (- 1)
     event_id (- 1)
   end
+
+  factory :invalid_arguments, parent: :trip do
+    id nil
+    date nil
+    hour nil
+    starting_point nil
+    destination_id nil
+    event_id nil
+  end
 end
 
