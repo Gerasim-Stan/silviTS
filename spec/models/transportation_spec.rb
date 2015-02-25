e4dc2d884ef6300b1aa667fc160f9de92ce5a68d
Gero <gero@tutuf.com>
2015-02-25 14:05:32 +0200

    Add tests for valid transportation

--- spec/models/transportation_spec.rb
+++ spec/models/transportation_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe Transportation, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Transportation do
+  let(:transportation) { FactoryGirl.create(:transportation) }
+
+  context "valid transportation" do
+  	it "has valid factory" do
+  	  expect(transportation.valid?).to be_truthy
+  	end
+
+  	it "has description" do
+  	  transportation.description = nil
+  	  expect(transportation.valid?).to be_falsey
+	end
+
+	it "description's length >= 10" do
+	  transportation.description = 'Ten chars.'
+	  expect(transportation.valid?).to be_truthy
+	end
+
+	it "has priority" do
+	  transportation.priority = nil
+	  expect(transportation.valid?).to be_falsey
+	end
+
+	it "accepts only integer for priority" do
+	  expect(transportation.priority).to be_instance_of Fixnum
+	end
+
+	it "priority's value is natural number" do
+	  expect(transportation.priority.to_i).to be > 0
+	end
+  end
 end
