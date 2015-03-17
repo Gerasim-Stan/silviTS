d161bdf120effdb07e780e2583891bd4a4ee6ed7
Gero <gero@tutuf.com>
2015-03-17 18:00:23 +0200

    Add precompiled files for heroku and run rake rails:update

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
-# Load the rails application
+# Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-# Initialize the rails application
-SilviTS::Application.initialize!
+# Initialize the Rails application.
+Rails.application.initialize!
