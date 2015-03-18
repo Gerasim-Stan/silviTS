e3066cb880598bb5e47171eafad60eba7e888b76
Gero <gero@tutuf.com>
2015-03-18 13:50:06 +0200

    Fix typo in destinations_path in requests test

--- spec/requests/destinations_spec.rb
+++ spec/requests/destinations_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Destinations", :type => :request do
   describe "GET /destinations" do
     it "works! (now write some real specs)" do
-      get destinations_index_path
+      get destinations_path
       expect(response).to have_http_status(200)
     end
   end
