e2b47b0337a358ab3cb32fc5d89180241bbad23e
Gero <gero@tutuf.com>
2015-03-25 16:06:54 +0200

    Add initializing methods in features helper

--- spec/support/features_helper.rb
+++ spec/support/features_helper.rb
@@ -7,9 +7,31 @@ module FeaturesHelper
     click_button "Create Destination"
   end
 
-  def remove_the_first_found_destroy_button
-    visit '/destinations'
+  def remove_the_first_found_destroy_button(path)
+    visit path
     first(:button, 'Destroy').click
     page.driver.browser.switch_to.alert.accept
+  end
+
+  def create_new_event(event_name: 'Приятелска програма', event_description: 'Готино описание на приятелска програма.')
+    visit "/events"
+    click_button "New Event"
+    fill_in "event_name", with: event_name
+    fill_in "event_description", with: event_description
+    click_button "Create Event"
+  end
+
+  def select_dropdown_option(id, value)
+    select "#{value}", :from => "#{id}"
+  end
+
+  def create_new_trip(trip_hour: '8:45', trip_starting_point: "БНБ")
+    visit "/trips"
+    click_button "New Trip"
+    fill_in "trip_hour"          , with: trip_hour
+    fill_in "trip_starting_point", with: trip_starting_point
+    select_dropdown_option('trip_destination_id', Destination.all.sample.name)
+    select_dropdown_option('trip_event_id', Event.all.sample.name)
+    click_button "Create Trip"
   end
 end
